c2305e816862e35274b8a12497df83a8fd7c9bbb
Isaias Medina <isaias@rombhus.com>
2023-08-24 12:33:07 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -2670,8 +2670,8 @@ function premioindi(){
 
 function db($sql){
 	//echo $sql;
-	//$mysqli = new mysqli('localhost', 'lebasi_app', 'Lactoserum22###', 'lebasi_app');
-	$mysqli = new mysqli('localhost:10016', 'root', 'root', 'lebasi_app');
+	$mysqli = new mysqli('localhost', 'lebasi_app', 'Lactoserum22###', 'lebasi_app');
+	//$mysqli = new mysqli('localhost:10016', 'root', 'root', 'lebasi_app');
 	
 	mysqli_set_charset($mysqli, 'utf8');
 	if(!$mysqli) {
